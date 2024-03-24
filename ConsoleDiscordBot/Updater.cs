d587445fcad34b96017c7f0ce27b6d1989b80c1d
Marco-Zechner <marcoflorian00956@gmail.com>
2024-03-24 16:40:05 +0100

    Testing filePaths on Linux Server

--- ConsoleDiscordBot/Updater.cs
+++ ConsoleDiscordBot/Updater.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using System.Reflection;
 
 namespace ConsoleDiscordBot
 {
@@ -9,15 +10,19 @@ namespace ConsoleDiscordBot
         // major - a feature completed
         // minor - new features added
         // hotfix - bug fixes, small changes
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         [SlashCommand("BeamMeUpScotty", "Tries to update the Bot")]
         public static async Task UpdateBot(InteractionContext ctx)
         {
             await ctx.DeferAsync(true);
 
+            ulong channelID = ctx.Channel.Id;
+            
+            string exePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
             await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent($"Bot currently on Version {Version}. Bot will now attempt to restart and update."));
+                .WithContent($"Bot currently on Version {Version}. Bot will now attempt to restart and update.\n{channelID}\n{exePath}"));
             
             Environment.Exit(1);
         }
