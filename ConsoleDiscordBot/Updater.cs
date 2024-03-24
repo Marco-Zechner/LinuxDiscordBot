a3b077340d8dce5b9db487d33b6e9ea8e75da803
Marco-Zechner <marcoflorian00956@gmail.com>
2024-03-24 16:27:48 +0100

    Added Version Info to the Updater
    - v1.0.0

--- ConsoleDiscordBot/Updater.cs
+++ ConsoleDiscordBot/Updater.cs
@@ -5,15 +5,30 @@ namespace ConsoleDiscordBot
 {
     public class Updater : ApplicationCommandModule
     {
+        // major.minor.hotfix
+        // major - a feature completed
+        // minor - new features added
+        // hotfix - bug fixes, small changes
+        public const string Version = "1.0.0";
+
         [SlashCommand("BeamMeUpScotty", "Tries to update the Bot")]
         public static async Task UpdateBot(InteractionContext ctx)
         {
             await ctx.DeferAsync(true);
 
             await ctx.EditResponseAsync(new DiscordWebhookBuilder()
-                .WithContent("Bot will close now."));
+                .WithContent($"Bot currently on Version {Version}. Bot will now attempt to restart and update."));
             
             Environment.Exit(1);
+        }
+
+        [SlashCommand("VersionInfo", "Prints the current Version of the Bot.")]
+        public static async Task VersionInfo(InteractionContext ctx)
+        {
+            await ctx.DeferAsync(true);
+
+            await ctx.EditResponseAsync(new DiscordWebhookBuilder()
+                .WithContent($"Bot currently on Version {Version}."));
         }
     }
 }
